63fff12a26f8a2090ef79f0fad4b4dc56dd5e5bb
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2020-09-29 13:15:35 -1000

    feat: alias for several app areas enabling clear pathing fix: boilerplate's babel configurations now use the config.js option and react-native-dotenv is available for non-expo built solutions.

--- src/boilerplate.ts
+++ src/boilerplate.ts
@@ -200,7 +200,7 @@ And here: https://guides.cocoapods.org/using/getting-started.html
     { template: ".env.example", target: ".env" },
     { template: ".prettierignore", target: ".prettierignore" },
     { template: ".solidarity", target: ".solidarity" },
-    { template: "babel.config.js", target: "babel.config.js" },
+    { template: "babel.config.js.ejs", target: "babel.config.js" },
     { template: "react-native.config.js", target: "react-native.config.js" },
     { template: "tsconfig.json", target: "tsconfig.json" },
     { template: "app/app.tsx.ejs", target: "app/app.tsx" },
