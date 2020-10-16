ad62007ad129960b1d85190b897ffe8a689906f8
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2020-10-16 11:24:53 -1000

    feat: ProjectInfo added for checking what is enabled on the given project. Very helpful for generators (see example in the component generator)

---
+++ src/lib/ProjectInfo.ts
@@ -0,0 +1,24 @@
+import { read } from "fs-jetpack"
+import { PackageJSON } from "gluegun/build/types/toolbox/meta-types"
+import { isEmpty, isNil } from "lodash"
+
+export const getDepVersion = (info: PackageJSON, dependencyKey: string) => {
+  return info.dependencies[dependencyKey]
+}
+
+/**
+ * Designed specifically for an already built project
+ */
+export default {
+  getDepVersion,
+  hasMST () {
+    const info = JSON.parse(read(process.cwd() + '/package.json'))
+    const version = getDepVersion(info, 'mobx')
+    return !isNil(version) && !isEmpty(version)
+  },
+  hasNativeBase () {
+    const info = JSON.parse(read(process.cwd() + '/package.json'))
+    const version = getDepVersion(info, 'native-base')
+    return !isNil(version) && !isEmpty(version)
+  }
+}
