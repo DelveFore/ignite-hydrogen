b18dffe1bbcbccb1642b7758edf66163aad6038a
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2020-10-19 14:52:26 -1000

    Make it easier to test connect state by moving up the ejs

---
+++ boilerplate/app/state/models/connect.ts
@@ -0,0 +1,9 @@
+import { observer } from "mobx-react-lite"
+
+/**
+ * While the primary reason is to make Hydrogen easier to maintain,
+ *  there is an added benefit of centralizing State Machine connection.
+ */
+export default (Component, options = {}) => {
+  return observer(Component)
+}
