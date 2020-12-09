33f503485916a1e0ad6e1dfad1adcb1042c0ef04
Jonno <jonno.arcus@delvefore.com>
2020-12-08 14:55:59 -1000

    doc: removing TODOs

--- boilerplate/app/screens/createScreen.ts
+++ boilerplate/app/screens/createScreen.ts
@@ -1,5 +1,4 @@
 import { connectState } from "@app/state"
-// TODO Support NativeBase alternative
 import { connectStyle } from "native-base"
 export default (name: string, Component: any) => {
   return connectStyle(name + "Screen", {})(connectState(Component))
@@ -8,7 +7,6 @@ export default (name: string, Component: any) => {
 /*
 import { connectState } from '@app/state'
 import { withTheme } from "../theming"
-// TODO Support NativeBase alternative
 export default (name: string, Component: any) => {
   return connectState(withTheme(Component))
 }
