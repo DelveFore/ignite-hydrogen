605930e12a69a5bb246d4ca02ddd95ccd2bf19d6
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2020-12-10 08:55:10 -1000

    feat: color is now colors to more appropriately reflect the plurality usage

--- boilerplate/app/theming/paper/theme.ts
+++ boilerplate/app/theming/paper/theme.ts
@@ -1,14 +1,14 @@
-import color from '../../theme/color'
-import { DefaultTheme } from 'react-native-paper';
+import { colors } from '../../theme'
+import { DefaultTheme } from 'react-native-paper'
 
 export default {
   ...DefaultTheme,
   dark: false,
   colors: {
     ...DefaultTheme.colors,
-    primary: color.primary,
-    accent: '#03dac4',
-    background: color.background,
-    text: color.text
+    primary: colors.primary,
+    accent: '#03dac4', // TODO Add color to `colors` for `accent` and `secondary`
+    background: colors.background,
+    text: colors.text
   }
 }
