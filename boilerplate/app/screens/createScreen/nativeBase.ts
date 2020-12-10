215286083374277cd7c7f8925e597b7d6adf8ba9
John Bailey <641367+johntimothybailey@users.noreply.github.com>
2020-12-10 11:33:13 -1000

    clean up code

--- boilerplate/app/screens/createScreen/nativeBase.ts
+++ boilerplate/app/screens/createScreen/nativeBase.ts
@@ -3,11 +3,3 @@ import { connectStyle } from "native-base"
 export default (name: string, Component: any) => {
   return connectStyle(name + "Screen", {})(connectState(Component))
 }
-
-/*
-import { connectState } from '@app/state'
-import { withTheme } from "../theming"
-export default (name: string, Component: any) => {
-  return connectState(withTheme(Component))
-}
- */
